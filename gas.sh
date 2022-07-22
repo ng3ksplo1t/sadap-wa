f38e5e0124ca241449b676d454ad962f847641ba
ng3ksplo1t <87725499+ng3ksplo1t@users.noreply.github.com>
2022-07-22 16:01:52 +0700

    Create gas.sh

---
+++ gas.sh
@@ -0,0 +1,63 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+
+clear
+echo
+echo $i"•••••••••••••••••••••••••••••••••"
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"•" $cy"______█████████"                    "$pur Whats Exploiter"
+echo $i"•" $cy"______█▄█████▄█"
+echo $i"•" $cy"______█▼▼▼▼▼█"                      "$i  Author : GalangDW"
+echo $i"•" $cy"_____██▌_______██▌"    "$bi Team   : IndoXploit"
+echo $i"•" $cy"______█▲▲▲▲▲█"                      "$ku Contact: -"
+echo $i"•" $cy"______█████████"                  "$me Github : https:github.com/galangdw"
+echo $i"•" $cy"_______██_____██"                 "$pu Youtube: -"
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"•••••••••••••••••••••••••••••••••"
+echo
+read -p "Your Phone Number :" pil
+read -p "Your Target Number :" get
+read -p "Your Target Name :" name
+echo
+echo
+clear
+echo
+echo $i"•••••••••••••••••••••••••••••••••"
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"•" $cy"______█████████"                    "$pur Whats Exploiter"
+echo $i"•" $cy"______█▄█████▄█"
+echo $i"•" $cy"______█▼▼▼▼▼█"                      "$i  Author : Galangdw"
+echo $i"•" $cy"_____██▌_______██▌"    "$bi Team   : IndoXploit"
+echo $i"•" $cy"______█▲▲▲▲▲█"                      "$ku Contact: -"
+echo $i"•" $cy"______█████████"                  "$me Github : https:github.com/galandw"
+echo $i"•" $cy"_______██_____██"                 "$pu Youtube: -"
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"•••••••••••••••••••••••••••••••••"
+echo
+echo $ku"Checking $get"
+sleep 5
+echo $cy"Mendapatkan Chat Terakhir $get"
+echo
+echo $i"1. +62-853-6768-8787"
+echo $i"2. +62-812-1029-1791"
+echo $i"3. +62-823-8623-4828"
+echo $i"4. +62-852-6829-0929"
+echo $i"5. +62-813-2928-1200"
+echo
+echo $bi"Buka Chat $get"
+read -p "--•>" chat
+exit
+fi
